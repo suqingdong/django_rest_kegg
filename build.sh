ce03d9a17832827a3adc55960d4a0809ee03a97e
suqingdong <suqingdong@novogene.com>
2021-09-30 12:15:35 +0800

    add setup

---
+++ build.sh
@@ -0,0 +1,7 @@
+find django_rest_kegg -name '*_initial.py' -exec rm -f {} \;
+
+rm -rf build dist *.egg-info
+python3 setup.py build sdist bdist_wheel
+python3 -m twine check dist/*
+
+echo 'python3 -m upload check dist/*'
